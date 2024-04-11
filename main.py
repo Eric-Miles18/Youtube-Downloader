1fb3dbc14688311738f61de0c0f4a396043af8ca
Eric <ericmiles199@gmail.com>
2024-04-11 08:37:23 +0300

    Now can download play list and make the code better

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 from pytube import *
 
+runState = True
+path = str(input("Enter the path you want to save on: "))
+path = path + "/"
 
-link = str(input("Enter the url of the vedio: "))
-yt = YouTube(link)
+def Video():
+    link = str(input("Enter the url of the vedio: "))
+    video = YouTube(link)
+    print("Downloading: " + video.title)
+    file = path + video.title
 
-print("The title of the viedo is: "+ yt.title + "\n")
+    for i in video.streams.filter(progressive=True):
+        print(i)
 
-for i in yt.streams.filter(progressive=True):
-    print(i)
+    itag = int(input("Enter the itag number of the res you want to download: "))
+    stream = video.streams.get_by_itag(itag)
 
-itag = int(input("Enter the itag number of the res you want to download: "))
-stream = yt.streams.get_by_itag(itag)
+    stream.download(output_path=file)
+    print("Done!")
 
-stream.download(output_path="C:/Users/Eric/Downloads/YT")
-print("Finished downloading")
+def playList():
+    link = str(input("Enter the url of the playlist: "))
+    playlist = Playlist(link)
+    print("Downloading: " + playlist.title)
+    file = path + playlist.title
+
+    count = 0
+
+    for video in playlist.videos:
+        video.streams.first().download(output_path=file)
+        count = count + 1
+        print(count)
+    
+    print("Done!")
+
+
+while runState == True:
+    print("Single video = 1 \nPlaylist = 2\nExit = 0")
+    userInput = int(input("What are you going to download using this tool: "))
+
+    if userInput == 1:
+        Video()
+    elif userInput == 2:
+        playList()
+    elif userInput == 0:
+        runState = False
+    else:
+        print("Enter a valid input")
